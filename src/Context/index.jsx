680395deeea11d2d7aca824e781272468d94ca4e
nicolas.nos <nicolas.nos@hotmail.com>
2024-02-15 15:20:01 -0500

    feature: se crea un manejador para la cantidad de tipificaciones

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, createContext } from 'react'
-import { tempData } from '../../assets/tempData'
+import { tempData } from '../assets/tempData'
+import { dataArray } from '../assets/Motivos'
 
 const FormularioContext = createContext()
 
@@ -121,21 +122,35 @@ function FormularioProvider({ children }) {
       name: 'Información general',
     },
   ]
-
+  // data inicial del componente metadata
   const initialData = {
     docType: '',
     docNum: 0,
   }
+  //data inicial del componente tipifications
+  const initialTip = {
+    motivo: '',
+  }
 
   //Estado inicial de metadata
   const [metaData, setMetaData] = useState(initialData)
-  //Estado inicial de la data pagina 2
+  //Estado inicial de la data pagina autorizaciones
   const [autData, setAutData] = useState()
-  //Estado inicial de la data pagina 3
-  const [tipData, setTipData] = useState([])
+  //Estado inicial de la data en tipificaciones
+  const [tipData, setTipData] = useState([initialTip])
   //Array de usuarios encontrados
   const [filteredUser, setFilteredUser] = useState(undefined)
+  //cantidad de tipificaciones a mapear
+  const [tipificationsLines, setTipificationsLines] = useState(1)
 
+  //array del nivel 1
+  const [nivel1, setNivel1] = useState([])
+  //array del nivel 2
+  const [nivel2, setNivel2] = useState([])
+  //array del nivel 3
+  const [nivel3, setNivel3] = useState([])
+
+  //funcion que limpia la información los input
   const cleanData = (e, num) => {
     e.preventDefault()
     switch (num) {
@@ -152,6 +167,7 @@ function FormularioProvider({ children }) {
     }
   }
 
+  // funcion que busca el usuario por el número de documento
   const findUser = (numeroId) => {
     const sendNumber = numeroId
     const result = tempData.find(({ numeroId }) => sendNumber === numeroId)
@@ -163,19 +179,64 @@ function FormularioProvider({ children }) {
       return
     }
   }
+  //logica que agrega en uno hasta máximo 10 el valor de las tipificaciones
+  const addLine = () => {
+    if (tipificationsLines < 10) {
+      setTipificationsLines(tipificationsLines + 1)
+    }
+  }
+  //aumenta el valor del array con el que se imprimen las tipificaciones
+  const formLines = () => {
+    const newArray = []
+    for (let index = 0; index < tipificationsLines; index++) {
+      newArray.push(index)
+    }
+    return newArray
+  }
+
+  // filtro del nivel 1
+  const filtNiv1 = (valor) => {
+    const valorEnviado = valor
+    const result = dataArray.find(({ texto }) => valorEnviado === texto)
+    setNivel1(result.nivel1)
+  }
+  // filtro del nivel 2
+  const filtNiv2 = (valor) => {
+    const valorEnviado = valor
+    const result = nivel1.find(({ texto }) => valorEnviado === texto)
+    console.log(result)
+    setNivel2(result.nivel2)
+  }
+  // filtro del nivel 3
+  const filtNiv3 = (valor) => {
+    const valorEnviado = valor
+    const result = nivel2.find(({ texto }) => valorEnviado === texto)
+    setNivel3(result.nivel3)
+  }
 
   return (
     <FormularioContext.Provider
       value={{
         metaData,
+        initialTip,
         setMetaData,
         setAutData,
         setTipData,
+        tipificationsLines,
         documentTypeList,
         serviceTypeList,
         cleanData,
+        addLine,
         findUser,
         filteredUser,
+        formLines,
+        filtNiv1,
+        nivel1,
+        filtNiv2,
+        nivel2,
+        filtNiv3,
+        nivel3,
+        tipData,
       }}
     >
       {children}
---
+++ src/assets/Motivos.js
@@ -0,0 +1,474 @@
+export const dataArray = [
+  {
+    texto: 'Autorizaciones',
+    nivel1: [
+      {
+        texto: 'Solicitud y consulta de autorizaciones',
+        nivel2: [
+          {
+            texto: 'Autorización de servicios médicos mayor de 60 años',
+            nivel3: [],
+          },
+          {
+            texto: 'Autorización de medicamentos  mayor de 60 años',
+            nivel3: [],
+          },
+          {
+            texto: 'Demora autorización / Conector caso 30 min',
+            nivel3: [],
+          },
+          {
+            texto: 'Incidencia en Autorización / No prioritario',
+            nivel3: [],
+          },
+          {
+            texto: 'Incidencia en Autorización / Respuesta inmediata',
+            nivel3: [],
+          },
+          {
+            texto: 'Renovación de Autorización',
+            nivel3: [],
+          },
+          {
+            texto: 'Solicitud de autorización (Menores de 60 años)',
+            nivel3: [
+              {
+                texto:
+                  'Solicitud de autorización prestaciones (Servicios Médicos)',
+              },
+              {
+                texto: 'Solicitud de autorización medicamentos',
+              },
+            ],
+          },
+          {
+            texto: 'Consulta de autorizaciones (Menores de 60 años)',
+            nivel3: [
+              {
+                texto: 'Consulta de autorizaciones - Prestaciones',
+              },
+              {
+                texto: 'Consulta de autorizaciones - Medicamentos',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        texto: 'Consumo de medicamentos',
+        nivel2: [
+          {
+            texto: 'Servicios médicos',
+            nivel3: [
+              {
+                texto: 'Incidencia en Autorización de Medicamentos',
+              },
+              {
+                texto:
+                  'Información Post egreso hospitalario y Urgencias (medicamentos)',
+              },
+            ],
+          },
+          {
+            texto: 'Novedades con Cruz Verde',
+            nivel3: [
+              {
+                texto: 'Medicamentos agotados o descontinuados',
+              },
+              {
+                texto: 'Medicamentos no entregados',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Acompañamiento canal virtual',
+    nivel1: [
+      {
+        texto: 'Zona pública',
+        nivel2: [
+          {
+            texto: 'Consultas y transacciones',
+            nivel3: [
+              {
+                texto: 'Portal',
+              },
+              {
+                texto: 'App',
+              },
+              {
+                texto: 'Chatbot - María Paula',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        texto: 'Zona privada',
+        nivel2: [
+          {
+            texto: 'Consultas y transacciones',
+            nivel3: [
+              {
+                texto: 'Portal',
+              },
+              {
+                texto: 'App',
+              },
+              {
+                texto: 'Chatbot - María Paula',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Actualización de datos de contacto - BH',
+    nivel1: [
+      {
+        texto: 'Actualización de datos personales',
+        nivel2: [
+          {
+            texto: 'Actualización de datos de contacto',
+            nivel3: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Certificados',
+    nivel1: [
+      {
+        texto: 'Certificado para efectos tributarios',
+        nivel2: [
+          {
+            texto: 'Generación y/o envío de cerficado de retefuente y pagos',
+            nivel3: [],
+          },
+          {
+            texto: 'Trámite en línea MP - Radicación de Conector',
+            nivel3: [],
+          },
+        ],
+      },
+      {
+        texto:
+          'Generación de certificado de seguro de viaje (Consular-Migratorio)',
+        nivel2: [
+          {
+            texto: 'Envío certificado cobertura internacional',
+            nivel3: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Consultas',
+    nivel1: [
+      {
+        texto: 'Consulta de factura (Consulta y/o envío)',
+        nivel2: [
+          {
+            texto: 'Generación y/o envío factura',
+            nivel3: [],
+          },
+        ],
+      },
+      {
+        texto: 'Historial de pagos',
+        nivel2: [
+          {
+            texto: 'Consulta pago cuenta o pago en línea PSE',
+            nivel3: [],
+          },
+        ],
+      },
+      {
+        texto:
+          'Resultado de la última transacción (Pago de factura realizada en el canal María Paula',
+        nivel2: [
+          {
+            texto:
+              'Transacción en zona de pagos no regista / Transacción en zona de pagos pendiente',
+            nivel3: [],
+          },
+        ],
+      },
+      {
+        texto: 'Novedades de cartera',
+        nivel2: [
+          {
+            texto:
+              'Acompañamiento para radicación y consulta de novedades de cartera (Portal de usuarios de MP)',
+            nivel3: [
+              {
+                texto: 'Valores facturados',
+              },
+              {
+                texto:
+                  'Inscripción de cuenta bancaria para pago (Débito bancario)',
+              },
+              {
+                texto: 'Inconsistencias en el pago',
+              },
+              {
+                texto: 'Cambiar periodicidad de facturación',
+              },
+              {
+                texto: 'Radicación de Conector por novedades de cartera',
+              },
+            ],
+          },
+          {
+            texto: 'Liquidador pago anticipado',
+            nivel3: [
+              {
+                texto: 'Consultas y novedades de pago anticipado',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        texto: 'Consulta de mis planes',
+        nivel2: [
+          {
+            texto: 'Solicitud de inclusiones o nuevas afiliaciones',
+            nivel3: [
+              {
+                texto:
+                  'Envío de información al formulario de Gcia. Comercial (SalesForce)',
+              },
+            ],
+          },
+          {
+            texto: 'Novedades al contrato',
+            nivel3: [
+              {
+                texto: 'Solicitud de cancelaciones',
+              },
+              {
+                texto: 'Certificados de antigüedad',
+              },
+              {
+                texto: 'Certificados de preexistencias',
+              },
+              {
+                texto: 'Certificado de utilizaciones',
+              },
+              {
+                texto: 'Radicación de Conector',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Citas médicas Banrep',
+    nivel1: [
+      {
+        texto:
+          'Citas Banrep (Incluye consulta externa con todas sus especialidades: odontología, optometría, imágenes Dx y Px Dx',
+        nivel2: [
+          {
+            texto: 'Médico puerta de entrada',
+            nivel3: [
+              {
+                texto: 'Asig. y/o confir. Cita puerta de entrada',
+              },
+              {
+                texto: 'Canc. y/o traslado cita Médico puerta de entrada',
+              },
+              {
+                texto: 'Canc. y/o traslado cita Odontólogo Puerta de Entrada',
+              },
+            ],
+          },
+          {
+            texto: 'Preagendamiento',
+            nivel3: [
+              {
+                texto: 'Asig. y/o confir. Cita especialista (preagendamiento)',
+              },
+              {
+                texto: 'Canc. y/o traslado cita especialista (preagendamiento)',
+              },
+            ],
+          },
+          {
+            texto: 'Canc. y/o traslado cita Puerta de Entrada',
+            nivel3: [
+              {
+                texto: 'Médico Puerta de Entrada',
+              },
+              {
+                texto: 'Odontólogo Puerta de Entrada',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Vales electrónicos',
+    nivel1: [
+      {
+        texto: 'Consulta vales (Disponibles y consumidos)',
+        nivel2: [
+          {
+            texto:
+              'Compra de vales - Consulta en pasarela de pagos (Paymentez)',
+            nivel3: [
+              {
+                texto: 'Consulta de transacciones - Datáfono Colsanitas',
+              },
+              {
+                texto: 'Consulta de Transacciones - Datáfono Medisanitas',
+              },
+              {
+                texto:
+                  'Consulta de transacciones - Corresponsal bancario Colsanitas',
+              },
+              {
+                texto:
+                  'Consulta de transacciones - Corresponsal bancario Medisnaitas',
+              },
+            ],
+          },
+          {
+            texto: 'Consulta de Vales - MFT de Recaudo',
+            nivel3: [
+              {
+                texto: 'Vales Consumidos',
+              },
+              {
+                texto: 'Vales Habilitados',
+              },
+              {
+                texto: 'Vale comprado para otro contrato',
+              },
+              {
+                texto: 'Consulta de Prestador que Redimió el Vale',
+              },
+            ],
+          },
+          {
+            texto: 'Incidencia Vales Electrónicos',
+            nivel3: [
+              {
+                texto:
+                  'Demora en la asignación - Transacción Pendiente en  la Pasarela de Pagos',
+              },
+              {
+                texto:
+                  'Demora en la asignación - Aprobada en Pasarela pero no en Replicador Tecnología Keralty',
+              },
+              {
+                texto:
+                  'Demora en la asignación - Incidencia en Transacciones Pasarela ACH / TDC',
+              },
+              {
+                texto: 'No registra Vale',
+              },
+              {
+                texto: 'Radicación de Conector a  Recaudo',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Resultados médicos',
+    nivel1: [
+      {
+        texto: 'Consulta de imágenes diagnósticas',
+        nivel2: [
+          {
+            texto: 'Consulta de resultados de imágenes diagnósticas',
+            nivel3: [],
+          },
+          {
+            texto:
+              'Envío de resultados de imágenes díagnosticas por novedad de la OFV (Por Excepción)',
+            nivel3: [],
+          },
+        ],
+      },
+      {
+        texto: 'Consulta de resultados de laboratorio',
+        nivel2: [
+          {
+            texto: 'Consulta de resultados de laboratorios',
+            nivel3: [],
+          },
+          {
+            texto:
+              'Envío de resultados de laboratorios por novedad de la OFV (Por Excepción)',
+            nivel3: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Directorios',
+    nivel1: [
+      {
+        texto: 'Directorio de oficinas',
+        nivel2: [
+          {
+            texto: 'Información directorio de oficinas',
+            nivel3: [],
+          },
+        ],
+      },
+      {
+        texto: 'Directorio médico',
+        nivel2: [
+          {
+            texto: 'Información Directorio Médico',
+            nivel3: [],
+          },
+          {
+            texto: 'Reporte de  Novedades de  Directorio Médico',
+            nivel3: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    texto: 'Seguro de viaje',
+    nivel1: [
+      {
+        texto: 'Activación de seguro de viaje',
+        nivel2: [
+          {
+            texto: 'Activación WTA',
+          },
+        ],
+      },
+      {
+        texto: 'Consulta de activación de seguro de viaje',
+        nivel2: [
+          {
+            texto: 'Consulta WTA',
+            nivel3: [],
+          },
+        ],
+      },
+    ],
+  },
+]
